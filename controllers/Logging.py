003b34673ce16f0542ef6b795d8ad940f580d708
Node <jordansavell1198@gmail.com>
2019-05-19 20:50:48 +0100

    get basic logging setup

--- controllers/Logging.py
+++ controllers/Logging.py
@@ -1,2 +1,18 @@
-class Logging: # Custom logging
-    pass
+import logging
+
+class Logging:
+
+    def __init__(self):
+        self.log_directories = ['atar_panel_main.log']
+        self.logger = logging
+        self.logger.basicConfig(level=self.logger.DEBUG,
+                                format='%(asctime)s %(levelname)-8s %(message)s',
+                                datefmt='%d:%m:%y',
+                                filename='logs/' + self.log_directories[0],
+                                filemode='w+')
+
+    def main_logging(self):
+        pass
+
+    def controller_logging(self):
+        pass
