ec5ad1d5633dad78f3193e0e7d5b3a5ecfb6edfb
Node <jordansavell1198@gmail.com>
2019-05-19 14:56:00 +0100

    Added working command check and parser
    Need to add:
    > Multiple arguments
    > Logging feature

--- controllers/Commands.py
+++ controllers/Commands.py
@@ -1,7 +1,7 @@
 import os as system
 import shlex
 import subprocess as subproc # If you're going to use check_call, don't use stdour or stderr. Deadlocking threads is a no-go.
-from controllers import Files
+from controllers.files import Files
 
 """It's important to make sure we don't leave vulnerabilites here. Using system/os.stat
 will allow us to return bit permissions for a file. We don't want no SUID exploits."""
@@ -9,16 +9,20 @@ will allow us to return bit permissions for a file. We don't want no SUID exploi
 class Commands:
 
     invalid_commands = ['rm', 'rf', 'chmod'] # Input sanitization
-    command_line = input()
-    files = Files.Files
+    files = Files
 
     def __init__(self):
         pass
 
-    def get_command(self, command):
-        arguments = shlex.split(self.command_line)
-        if self.invalid_commands in arguments:
+    def get_command(self):
+        command_line = input()
+        if command_line in self.invalid_commands:
+            print("Command not allowed.") # This will eventually be logged, not output.
             exit()
+        else:
+            args = shlex.split(command_line)
+            print(subproc.Popen(args, shell=True))
 
-    def get_permissions(self):
-        system.stat(self.files.getFile())
+    def get_permissions(self, file):
+        file_permission = None
+        system.stat(file)
