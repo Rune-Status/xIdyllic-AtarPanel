71dc0ac6bda2f53e6ecb64d1063249c1ef0c5fa6
Node <jordansavell1198@gmail.com>
2019-05-19 20:50:31 +0100

    add some file functionality

--- controllers/Files.py
+++ controllers/Files.py
@@ -1,13 +1,16 @@
 from pathlib import Path
-import os
 from controllers import commands
+import shutil
 
+# This class will need to rely heavily on commands.py for checking permissions and such.
 
 class Files:
 
     def __init__(self):
         self.command_controller = commands
         self.path = None
+        self.file_util = shutil
+        self.path_util = Path
 
     def get_file(self, file):
         try:
@@ -25,11 +28,18 @@ class Files:
         except (FileNotFoundError, PermissionError, OSError) as err:
             print(err)  # log this
 
-    def copy_file(self):
+    def copy_file(self, file, desination_path):
+        try:
+            self.file_util.copyfile(file, desination_path)
+        except (PermissionError, OSError, FileNotFoundError) as err:
+            print(err)
+
+    def remove_file(self, file):
+        try:
+            self.path_util.unlink(file)
+        except (PermissionError, OSError, FileNotFoundError) as err:
+            print(err)
+
+    def backup_operation(self, directories):
         pass
 
-    def remove_file(self):
-        pass
-
-    def backup_file(self, files=None):
-        pass
