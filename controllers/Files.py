75efa7d2c25a73a0c5504eb5e142b7e9bd4d3b9d
Node <jordansavell1198@gmail.com>
2019-05-19 17:21:19 +0100

    added some methods and corrected current methods - now working.

--- controllers/Files.py
+++ controllers/Files.py
@@ -1,15 +1,35 @@
 from pathlib import Path
+import os
+from controllers import commands
 
 
 class Files:
 
     def __init__(self):
+        self.command_controller = commands
         self.path = None
 
-    def getFile(self, file):
-        if Path(file).is_file():
-            return file
+    def get_file(self, file):
+        try:
+            if Path(file).is_file():
+                print(file)
+                return
+        except (FileNotFoundError, PermissionError, OSError) as err:
+            print(err)  # log this
 
-    def getDir(self, directory):
-        if Path(directory).is_absolute():
-            return directory
+    def get_dir(self, directory):
+        try:
+            if Path(directory).is_absolute():
+                print(directory)
+                return
+        except (FileNotFoundError, PermissionError, OSError) as err:
+            print(err)  # log this
+
+    def copy_file(self):
+        pass
+
+    def remove_file(self):
+        pass
+
+    def backup_file(self, files=None):
+        pass
