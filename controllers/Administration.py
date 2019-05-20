63e178da7cd69f9dd198ac6866dd85183d1a8a4d
Node <jordansavell1198@gmail.com>
2019-05-20 16:20:43 +0100

    fixed the logic for using menu, will now add the ability to go back

--- controllers/Administration.py
+++ controllers/Administration.py
@@ -1,5 +1,6 @@
 from controllers import Files
 from controllers import Commands
+import time
 
 class Administration:
 
@@ -18,8 +19,10 @@ class Administration:
             6: 'Donation Logs',
             7: 'GE Logs',
             8: 'Item Pickup Logs',
-            9: 'Stake Logs'
+            9: 'Stake Logs',
+            10: 'Go Back'
         }
+
         self.server_menu = {
             1: 'Restart',
             2: 'Shutdown',
@@ -31,7 +34,7 @@ class Administration:
             self.main_menu[2],
             self.main_menu[3]
         )
-        self.user_men = '[1] {}\n[2] {}\n[3] {}\n[4] {}\n[5] {}\n[6] {}\n[7] {}\n[8] {}\n[9] {}\n'.format(
+        self.user_men = '[1] {}\n[2] {}\n[3] {}\n[4] {}\n[5] {}\n[6] {}\n[7] {}\n[8] {}\n[9] {}\n[10] {}\n'.format(
             self.user_log_menu[1],
             self.user_log_menu[2],
             self.user_log_menu[3],
@@ -40,13 +43,40 @@ class Administration:
             self.user_log_menu[6],
             self.user_log_menu[7],
             self.user_log_menu[8],
-            self.user_log_menu[9]
+            self.user_log_menu[9],
+            self.user_log_menu[10]
         )
+
+        self.serv_men = '[1] {}\n[2] {}\n[3] {}\n[4] {}\n'.format(
+            self.server_menu[1],
+            self.server_menu[2],
+            self.server_menu[3],
+            self.user_log_menu[10]
+        )
+
         self.file_controller = Files.Files
         self.command_controller = Commands.Commands
 
     def start_menu(self):
+        self.command_controller().clear_screen()
+        print(self.main_men)
+        user_input = int(input("Enter menu number >> "))
+        get_menu = self.main_menu.get(user_input, "Invalid input")
+        if get_menu == self.main_menu[1]:
+            pass #game logs
+        if get_menu == self.main_menu[2]:
+            self.user_menu()
+        if get_menu == self.main_menu[3]:
+            self.serv_menu()
+
+    def serv_menu(self):
+        self.command_controller().clear_screen()
+        print(self.serv_men)
         pass
 
+    def user_menu(self):
+        self.command_controller().clear_screen()
+        print(self.user_men)
+        pass
 
 
