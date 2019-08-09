6c78bde7f5750698e868790677572738e1fc36a4
Node <jordansavell1198@gmail.com>
2019-08-09 09:08:11 +0100

    Menu refactor

--- controllers/Menu.py
+++ controllers/Menu.py
@@ -49,19 +49,10 @@ class Menu:
  /_/   \_\\__|\__,_||_|   \__,_|/_/\_\|_| \__,_|  \____||_|    
 \nLatest Update: {}""".format(self.latest_commit))
         self.set_menu('main')
-        user_choice = self.get_user_choice()
 
-        if user_choice == 0:
+        if self.get_user_choice() == 0:
             self.command_controller().clear_screen()
             self.set_menu('game')
-
-    def set_menu(self, menu_type):
-        if menu_type == 'game':
-            self.game_menu()
-        elif menu_type == 'main':
-            self.main_menu()
-        elif menu_type == 'server':
-            self.serv_menu()
 
     def get_user_choice(self):
         return int(input("\nSelect menu number: "))
@@ -71,23 +62,23 @@ class Menu:
         input('Press enter to go back when you are ready. This will refresh the current session.')
         return self.game_menu()
 
-    def serv_menu(self):
-        for menu_number, menu_type in enumerate(self.server_menu):
-            print('[{}] - {}'.format(menu_number, menu_type))
-            continue
+    def set_menu(self, menu_select):
+        if menu_select == 'game':
+            self.print_menu_list(self.game_log_menu)
+            self.game_menu()
+        if menu_select == 'main':
+            self.print_menu_list(self.main_start)
+        if menu_select == 'server':
+            self.print_menu_list(self.server_menu)
 
-    def main_menu(self):
-        for menu_number, menu_type in enumerate(self.main_start):
+    def print_menu_list(self, menu_list):
+        for menu_number, menu_type in enumerate(menu_list):
             print('[{}] - {}'.format(menu_number, menu_type))
             continue
 
     def game_menu(self):
-        self.command_controller().clear_screen()
-        for menu_number, menu_type in enumerate(self.game_log_menu):
-            print('[{}] - {}'.format(menu_number, menu_type))
-            continue
-
         user_choice = self.get_user_choice()
+
         if user_choice == 0:
             self.get_log(self.file_controller().log_dirs[0])
         elif user_choice == 1:
